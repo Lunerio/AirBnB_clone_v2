01464874d1967f76459f128027f59997ecb7410a
Lunerio <lunerio@outlook.es>
2021-04-29 12:07:15 -0300

    first push

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module contains a Flask instance
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.run(host='0.0.0.0')
+
+
+@app.route('/', strict_slashes=False)
+def first_task():
+    """ This function returns a string
+    """
+    return 'Hello HBNB!'
