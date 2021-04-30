25e565cac1f57b1ea85e518711fded347087bdf0
Lunerio <lunerio@outlook.es>
2021-04-29 23:23:37 -0300

    save

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This script starts a web application. It takes requests
+    and generates a web page
+"""
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """ This function loads the list of states
+    and creates the web page
+    """
+    
+
+@app.teardown_appcontext
+def close_session():
+    """ This function closes a session
+    with the storage.close() method
+    """
+    storage.close()
